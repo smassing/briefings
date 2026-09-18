d62e0a7b41602a65eb3b15ce35e682d05a625051
Steve Massing <steve_massing@hotmail.com>
2026-09-18 09:57:08 -0600

    Publish daily briefing for 2026-09-18

--- briefings-index.js
+++ briefings-index.js
@@ -15,6 +15,7 @@ window.BRIEFING_FILES = [
   "daily/2026-09-04.html",
   "daily/2026-09-08.html",
   "daily/2026-09-10.html",
+  "daily/2026-09-18.html",
   "weekly/2026-08-21.html",
   "weekly/2026-08-28.html",
   "weekly/2026-09-04.html",
